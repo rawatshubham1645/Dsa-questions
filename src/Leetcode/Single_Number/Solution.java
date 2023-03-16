3e0fc90cb4c55ed66113376fa2775e8d7839b4a9
rawatshubham1645 <rawatshubham1645@gmail.com>
2023-03-16 23:21:36 +0530

    Single Number

---
+++ src/Leetcode/Single_Number/Solution.java
@@ -0,0 +1,12 @@
+package Leetcode.Single_Number;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int xor = 0;
+        for(int i: nums){
+            xor ^= i;
+        }
+        return xor;
+    }
+}
