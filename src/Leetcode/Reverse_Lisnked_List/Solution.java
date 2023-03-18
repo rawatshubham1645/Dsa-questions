058b6a739ff1b42d69ae8a043021d421b6ea1d8a
rawatshubham1645 <rawatshubham1645@gmail.com>
2023-03-19 00:20:30 +0530

    reverse Linked

---
+++ src/Leetcode/Reverse_Lisnked_List/Solution.java
@@ -0,0 +1,23 @@
+package Leetcode.Reverse_Lisnked_List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+	/*
+	 * public ListNode reverseList(ListNode head) { if(head==null)return head;
+	 * ListNode current = head; ListNode prev = null; ListNode Next = null;
+	 * 
+	 * while(current != null){ Next = current.next; current.next = prev; prev =
+	 * current; current = Next;
+	 * 
+	 * } head = prev; return prev; }
+	 */
+}
