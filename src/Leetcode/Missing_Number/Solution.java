eb65c58d641474905e8be70a95b2cc059a7c79a5
rawatshubham1645 <rawatshubham1645@gmail.com>
2023-03-19 23:37:55 +0530

    Missing Number

---
+++ src/Leetcode/Missing_Number/Solution.java
@@ -0,0 +1,15 @@
+package Leetcode.Missing_Number;
+
+import java.util.Arrays;
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]!=i)return i;
+            
+        } 
+        return nums.length;
+    }
+}
